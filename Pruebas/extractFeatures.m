f245365319ffd487b1dc784258d61eda3c0e973c
JaguarDMatt <cm.amaya10@uniandes.edu.co>
2017-05-28 17:41:49 -0500

    Informacion de training

--- Pruebas/extractFeatures.m
+++ Pruebas/extractFeatures.m
@@ -7,64 +7,48 @@ u=3;
 v=8;
 
 gaborArray = gaborFilterBank(u,v,39,39);
-info=cell(size(anottrain));
+%%
+info=cell(numel(anottrain),1);
+labels=cell(numel(anottrain),1);
 
+%%
+sample=2000;
+
+numl=size(anottrain,1);
 for i=1:numel(imtrain)
-I1=imread(imtrain{i});
-[I2,mask,ori]=preprorot(I1);
-[Mat,lab] = TeethAnnot( anottrain(:,i),size(I2),ori);
-
-[featureVector,gaborResult] = gaborFeatures(I2,gaborArray,1,1);
-[Gmag,Gdir]= imgradient(I2);
-I3=im2double(I2);
-
-for l=1:7
-
-for j=1:u
-for k=1:v
- 
+    I1=imread(imtrain{i});
+    [I2,mask,ori]=preprorot(I1);
+    [Mat,lab] = TeethAnnot( anottrain(:,i),size(I2),ori);
+    [featureVector,gaborResult] = gaborFeatures(I2,gaborArray,1,1);
+    [Gmag,Gdir]= imgradient(I2);
+    I3=im2double(I2);
+    for l=1:numl
+        matl=lab==l;
+        if(any(matl(:))==0)
+            continue;
+        else
+            int=I3(matl);
+            mag=Gmag(matl);
+            dir=Gdir(matl);
+            infoil=horzcat(int,mag,dir);
+            infoilf=zeros(numel(int),u*v);
+            for j=1:u
+                for k=1:v
+                    infoilf(:,(j-1)*v+k)=abs(gaborResult{u,v}(matl));
+                end
+            end
+            infos=horzcat(infoil,infoilf);
+            infos=datasample(infos,sample,1);
+            labels{(i-1)*numl+l}=ones(sample,1)*l;
+            info{(i-1)*numl+l}=infos;
+        end
+        display(100*((i-1)*numl+l)/(numl*numel(imtrain)));
+    end
 end
-end
-end
 
 
-%%
+%% Final
 
-
-%Features fondo
-fondo=lab==0;
-intensidad=I3(fondo);
-filtro1=abs(gaborResult{1,1}(fondo));
-filtro2=abs(gaborResult{1,2}(fondo));
-filtro3=abs(gaborResult{1,3}(fondo));
-filtro4=abs(gaborResult{1,4}(fondo));
-filtro5=abs(gaborResult{1,5}(fondo));
-
-intf=intensidad;
-fil1f=filtro1;
-fil2f=filtro2;
-fil3f=filtro3;
-fil4f=filtro4;
-fil5f=filtro5;
-
-%No fondo
-nfondo=lab~=3;
-intensidad=I3(nfondo);
-filtro1=abs(gaborResult{1,1}(nfondo));
-filtro2=abs(gaborResult{1,2}(nfondo));
-filtro3=abs(gaborResult{1,3}(nfondo));
-filtro4=abs(gaborResult{1,4}(nfondo));
-filtro5=abs(gaborResult{1,5}(nfondo));
-
-
-intn=intensidad;
-fil1n=filtro1;
-fil2n=filtro2;
-fil3n=filtro3;
-fil4n=filtro4;
-fil5n=filtro5;
-
-%%
-Y=vertcat(zeros(size(intf)),ones(size(intn)));
-X=vertcat(horzcat(intf,fil1f,fil3f,fil4f,fil5f),horzcat(intn,fil1n,fil3n,fil4n,fil5n));
-Mdl = fitcsvm(X,Y);
+data=cell2mat(info);
+lbls=cell2mat(labels);
+save('info.mat','data','lbls');
